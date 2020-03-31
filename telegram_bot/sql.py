a9915e2d0c84c1a7f5860a44b075472101800bf5
Ostap Kuch <ostapko220@gmail.com>
2020-03-31 16:52:27 +0300

    Fixed telegram authentication

--- telegram_bot/sql.py
+++ telegram_bot/sql.py
@@ -1,6 +1,9 @@
 import mysql.connector
 
-database = mysql.connector.connect(
+
+def database(query, params):
+    try:
+        connection = mysql.connector.connect(
             host="localhost",
             port="3306",
             user="safehouse",
@@ -8,14 +11,18 @@ database = mysql.connector.connect(
             database="safehouse1",
             auth_plugin='mysql_native_password'
         )
-
-
-def check_username(username):
-    try:
-        cursor = database.cursor()
-        sql = "SELECT nickname FROM telegram_bot WHERE nickname=%s"
-        cursor.execute(sql, [username])
-        database.close()
+        cursor = connection.cursor()
+        print(str(params))
+        cursor.execute(query, params)
         return cursor.fetchall()
     except mysql.connector.Error as err:
         print("Something went wrong: {}".format(err))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+
+def check_username(username):
+    sql = "SELECT nickname FROM telegram_bot WHERE nickname=%s"
+    return database(sql, [username])
